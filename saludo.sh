0144f3bc1a386d511dc3f58e5a1a52879a839573
Luis Carlos Jurado <luiscarjo157@gmail.com>
2026-05-27 11:01:27 -0500

    probando webhook y ngrow

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,4 @@
 echo "hola mundo con jenkins y github"
 echo "probando un triger desde jenkins y git"
 echo "Probar triger con periodos de tiempo"
+echo "probando trigger con webhook y usando ngrok"
